d38c32bc489a7f79af2afde197c12b18dd50659c
Santeri Hyvärinen <santeri.hyvarinen12@gmail.com>
2020-11-18 11:27:38 +0200

    Updated main.c

--- JTKJ_labra/main.c
+++ JTKJ_labra/main.c
@@ -21,7 +21,7 @@
 
 /* Board Header files */
 #include "Board.h"
-#include "kuva.h"
+//#include "kuva.h"
 #include "buzzer.h"
 
 #include "wireless/comm_lib.h"
@@ -36,7 +36,7 @@
 #define MUSICSTACK 1024
 
 Char labTaskStack[STACKSIZE];
-Char defaultStateStack[STALESTACK];
+//Char defaultStateStack[STALESTACK];
 Char displayStack[STACKSIZE];
 Char musicTaskStack[MUSICSTACK];
 // Char commTaskStack[STACKSIZE];
@@ -44,8 +44,8 @@ Char musicTaskStack[MUSICSTACK];
 static PIN_Handle hMpuPin;
 static PIN_State MpuPinState;
 static PIN_Config MpuPinConfig[] = {
-    Board_MPU_POWER  | PIN_GPIO_OUTPUT_EN | PIN_GPIO_HIGH | PIN_PUSHPULL | PIN_DRVSTR_MAX,
-    PIN_TERMINATE
+	Board_MPU_POWER  | PIN_GPIO_OUTPUT_EN | PIN_GPIO_HIGH | PIN_PUSHPULL | PIN_DRVSTR_MAX,
+	PIN_TERMINATE
 };
 
 static PIN_Handle buzzer;
@@ -54,13 +54,14 @@ static PIN_State buzzerState;
 
 PIN_Config buzzerConfig[] =
 {
-    Board_BUZZER | PIN_GPIO_OUTPUT_EN | PIN_PULLUP,
-    PIN_TERMINATE
+	// Vili: Tämä rivi on CC2650STK.c:stä
+	Board_BUZZER     | PIN_GPIO_OUTPUT_EN | PIN_GPIO_LOW | PIN_PUSHPULL | PIN_DRVSTR_MAX,
+	PIN_TERMINATE
 };
 
 static const I2CCC26XX_I2CPinCfg i2cMPUCfg = {
-    .pinSDA = Board_I2C0_SDA1,
-    .pinSCL = Board_I2C0_SCL1
+	.pinSDA = Board_I2C0_SDA1,
+	.pinSCL = Board_I2C0_SCL1
 };
 
 enum state{START, READ_SENSOR, UPDATE, NEW_MSG};
@@ -120,6 +121,10 @@ Void sensorTask(UArg arg0, UArg arg1)
 		if(myState == READ_SENSOR)
 		{
 			i2cMPU = I2C_open(Board_I2C, &i2cMPUParams);
+			if (i2cMPU == NULL)
+			{
+				System_abort("Error Initializing I2CMPU\n");
+			}
 
 			mpu9250_get_data(&i2cMPU, &ax, &ay, &az, &gx, &gy, &gz);
 			//sprintf(string, "x: %f | y: %f\n", gx, gy);
@@ -127,19 +132,23 @@ Void sensorTask(UArg arg0, UArg arg1)
 			if(gx > 80)
 			{
 				move = UP;
+				myState = UPDATE;
 			}
 			else if(gx < -80)
 			{
 				move = DOWN;
+				myState = UPDATE;
 			}
 			else if(gy > 80)
 			{
 
 				move = RIGHT;
+				myState = UPDATE;
 			}
 			else if(gy < -80)
 			{
 				move = LEFT;
+				myState = UPDATE;
 			}
 			else
 			{
@@ -148,20 +157,22 @@ Void sensorTask(UArg arg0, UArg arg1)
 			I2C_close(i2cMPU);
 
 		}
-		myState = UPDATE;
-		Task_sleep(200000/Clock_tickPeriod);
+		Task_sleep(100000/Clock_tickPeriod);
 	}
 }
 
-Void staleTask(UArg arg0, UArg arg1)
-{
+// Vili:
+// Tämä taski vie turhaan muistia. displayTask asettaa joka tapauksessa myState = READ_SENSOR
+/*Void staleTask(UArg arg0, UArg arg1)
+	{
 
 	if(myState == START)
 	{
-		myState = READ_SENSOR;
+	myState = READ_SENSOR;
 	}
 
-}
+	}*/
+
 void setMenuState()
 {
 	if(menuChoice <= QUIT && move == LEFT && menuChoice != PLAY)
@@ -185,37 +196,44 @@ Void displayTask(UArg arg0, UArg arg1)
 	Display_Params_init(&params);
 	params.lineClearMode = DISPLAY_CLEAR_BOTH;
 	Display_Handle displayHandle = Display_open(Display_Type_LCD, &params);
+	Display_clear(displayHandle); // Vili: Display kannattaa clearata, että siihen ei jää mitään kummittelemaan
+
+	// Vili: Siirsin grlibContextin luomisen alkuun, ettei sitä tehdä joka kerta
+	tContext *pContext = DisplayExt_getGrlibContext(displayHandle);
 
 	while(1)
 	{
 		if(myState == UPDATE && displayHandle && gameState == GAME)
 		{
-			tContext *pContext = DisplayExt_getGrlibContext(displayHandle);
 			if (pContext)
 			{
-			//	sprintf(str, "%s", disp_messages[move]);
+				//	sprintf(str, "%s", disp_messages[move]);
 				if(move == LEFT)
 				{
-					GrImageDraw(pContext, &arrowL, 0, 0);
-					GrFlush(pContext);
+					Display_print0(displayHandle, 5, 5, "LEFT");
+					//GrImageDraw(pContext, &arrowL, 0, 0);
+					//GrFlush(pContext);
 
 				}
 				else if(move == RIGHT)
 				{
-					GrImageDraw(pContext, &arrowR, 0, 0);
-					GrFlush(pContext);
+					Display_print0(displayHandle, 5, 5, "RIGHT");
+					//GrImageDraw(pContext, &arrowR, 0, 0);
+					//GrFlush(pContext);
 
 				}
 				else if(move == UP)
 				{
-					GrImageDraw(pContext, &arrowU, 0, 0);
-					GrFlush(pContext);;
+					Display_print0(displayHandle, 5, 5, "UP");
+					//GrImageDraw(pContext, &arrowU, 0, 0);
+					//GrFlush(pContext);;
 
 				}
 				else if(move == DOWN)
 				{
-					GrImageDraw(pContext, &arrowD, 0, 0);
-					GrFlush(pContext);
+					Display_print0(displayHandle, 5, 5, "DOWN");
+					//GrImageDraw(pContext, &arrowD, 0, 0);
+					//GrFlush(pContext);
 				}
 				else
 				{
@@ -232,20 +250,22 @@ Void displayTask(UArg arg0, UArg arg1)
 			if(menuChoice == MUTE)
 			{
 				Display_print0(displayHandle, 5, 5, "MUTE");
-				Task_sleep(500000/Clock_tickPeriod);
+				Task_sleep(1000000/Clock_tickPeriod);
 			}
 			else if(menuChoice == PLAY)
 			{
 				Display_print0(displayHandle, 5, 5, "PLAY");
-				Task_sleep(500000/Clock_tickPeriod);
+				Task_sleep(1000000/Clock_tickPeriod);
 			}
 			else if(menuChoice == QUIT)
 			{
 				Display_print0(displayHandle, 5, 5, "QUIT");
-				Task_sleep(500000/Clock_tickPeriod);
+				Task_sleep(1000000/Clock_tickPeriod);
 			}
 		}
 		myState = READ_SENSOR;
+		// Vili: Tämä pitää olla tässä että toinen saman prioriteetin taski pääsee pyörimään (musiikki)
+		Task_sleep(1000000/Clock_tickPeriod);
 
 	}
 }
@@ -273,13 +293,14 @@ Void displayTask(UArg arg0, UArg arg1)
 
 	}
 }
-*/
+ */
 Void musicTask(UArg arg0, UArg arg1) {
 	System_printf("started musicTask\n");
 	System_flush();
 	while(1) {
 		if (gameState == GAME) {
-		    buzzerOpen(buzzer);
+		//if (menuChoice == PLAY) { // Vili: Tällä voi kokeilla
+			buzzerOpen(buzzer);
 			buzzerSetFrequency(250); // d
 			Task_sleep(10000 / Clock_tickPeriod);
 
@@ -292,7 +313,7 @@ Void musicTask(UArg arg0, UArg arg1) {
 			buzzerSetFrequency(400); // d
 			Task_sleep(10000 / Clock_tickPeriod);
 
-		    buzzerClose();
+			buzzerClose();
 			Task_sleep(10000 / Clock_tickPeriod);
 		}
 		else {
@@ -304,30 +325,30 @@ Void musicTask(UArg arg0, UArg arg1) {
 /*
 Void commTaskFxn(UArg arg0, UArg arg1) {
 
-    // Radio to receive mode
+	// Radio to receive mode
 	int32_t result = StartReceive6LoWPAN();
 	if(result != true) {
 		System_abort("Wireless receive mode failed");
 	}
 
-    while (1) {
+	while (1) {
 
-        // If true, we have a message
-    	if (GetRXFlag() == true) {
+		// If true, we have a message
+		if (GetRXFlag() == true) {
 
-    		// Handle the received message..
-        }
+			// Handle the received message..
+		}
 
-    	// Absolutely NO Task_sleep in this task!!
-    }
+		// Absolutely NO Task_sleep in this task!!
+	}
 }
 */
 
 Int main(void) {
 
-    // Task variables
-	Task_Handle defaultState;
-	Task_Params defaultStateParams;
+	// Task variables
+	//Task_Handle defaultState;
+	//Task_Params defaultStateParams;
 
 	Task_Handle sensorTaskVar;
 	Task_Params labTaskParams;
@@ -342,77 +363,86 @@ Int main(void) {
 	//Task_Params menuParams;
 
 	/*
-	Task_Handle commTask;
-	Task_Params commTaskParams;
-	*/
+		 Task_Handle commTask;
+		 Task_Params commTaskParams;
+	 */
 
-    // Initialize board
-    Board_initGeneral();
-    Board_initI2C();
+	// Initialize board
+	Board_initGeneral();
+	Board_initI2C();
 
-    buzzer = PIN_open(&buzzerState, buzzerConfig);
-       if (buzzer == NULL)
-       {
-       	System_abort("Buzzer pin open failed!");
-       }
+	buzzer = PIN_open(&buzzerState, buzzerConfig);
+	if (buzzer == NULL)
+	{
+		System_abort("Buzzer pin open failed!");
+	}
 
+	// MPU stuff
+	hMpuPin = PIN_open(&MpuPinState, MpuPinConfig);
+	if (hMpuPin == NULL) {
+		System_abort("Pin open failed!");
+	}
 
-    /* Task */
-    Task_Params_init(&labTaskParams);
-    labTaskParams.stackSize = STACKSIZE;
-    labTaskParams.stack = &labTaskStack;
-    labTaskParams.priority = 3;
+	/* Task */
+	Task_Params_init(&labTaskParams);
+	labTaskParams.stackSize = STACKSIZE;
+	labTaskParams.stack = &labTaskStack;
+	labTaskParams.priority = 3;
 
-    Task_Params_init(&defaultStateParams);
-    defaultStateParams.stackSize = STALESTACK;
-    defaultStateParams.stack = &defaultStateStack;
-    defaultStateParams.priority = 3;
+	/*
+	Task_Params_init(&defaultStateParams);
+	defaultStateParams.stackSize = STALESTACK;
+	defaultStateParams.stack = &defaultStateStack;
+	defaultStateParams.priority = 3;
+	 */
 
-    Task_Params_init(&displayParams);
-    displayParams.stackSize = STACKSIZE;
-    displayParams.stack = &displayStack;
-    displayParams.priority = 1;
+	Task_Params_init(&displayParams);
+	displayParams.stackSize = STACKSIZE;
+	displayParams.stack = &displayStack;
+	displayParams.priority = 1;
 
 	Task_Params_init(&musicParams);
-    musicParams.stackSize = MUSICSTACK;
-    musicParams.stack = &musicTaskStack;
-    musicParams.priority = 1;
+	musicParams.stackSize = MUSICSTACK;
+	musicParams.stack = &musicTaskStack;
+	musicParams.priority = 1;
 
-    /*Task_Params_init(&menuParams);
-    menuParams.stackSize = MUSICSTACK;
-    menuParams.stack = &menuTaskStack;
-    menuParams.priority = 2; */
+	/*Task_Params_init(&menuParams);
+		menuParams.stackSize = MUSICSTACK;
+		menuParams.stack = &menuTaskStack;
+		menuParams.priority = 2; */
 
 	//Task_Params_init(&commTaskParams);
-    //commTaskParams.stackSize = STACKSIZE;
-    //commTaskParams.stack = &commTaskStack;
-    //commTaskParams.priority=1;
+	//commTaskParams.stackSize = STACKSIZE;
+	//commTaskParams.stack = &commTaskStack;
+	//commTaskParams.priority=1;
 
-    System_printf("Starting tasks\n");
-    System_flush();
+	System_printf("Starting tasks\n");
+	System_flush();
 
-    //labTask = Task_create(labTaskFxn, &labTaskParams, NULL);
-    //if (labTask == NULL) {
-    //	System_abort("Task create failed!");
-    //}
+	//labTask = Task_create(labTaskFxn, &labTaskParams, NULL);
+	//if (labTask == NULL) {
+	//	System_abort("Task create failed!");
+	//}
 
-    defaultState = Task_create(staleTask, &defaultStateParams, NULL);
-    if(defaultState == NULL)
-    {
-    	System_abort("Task create failed");
-    }
+	/*
+	defaultState = Task_create(staleTask, &defaultStateParams, NULL);
+	if(defaultState == NULL)
+	{
+		System_abort("Task create failed");
+	}
+	*/
 
-    sensorTaskVar = Task_create(sensorTask, &labTaskParams, NULL);
-    if (sensorTaskVar == NULL)
-    {
-    	System_abort("Task create failed");
-    }
+	sensorTaskVar = Task_create(sensorTask, &labTaskParams, NULL);
+	if (sensorTaskVar == NULL)
+	{
+		System_abort("Task create failed");
+	}
 
-    displayTaskVar = Task_create(displayTask, &displayParams, NULL);
-    if(displayTaskVar == NULL)
-    {
-    	System_abort("displayTask failed");
-    }
+	displayTaskVar = Task_create(displayTask, &displayParams, NULL);
+	if(displayTaskVar == NULL)
+	{
+		System_abort("displayTask failed");
+	}
 
 	musicTaskVar = Task_create(musicTask, &musicParams, NULL);
 	if (musicTaskVar == NULL)
@@ -421,24 +451,24 @@ Int main(void) {
 	}
 
 	/*menuTaskVar = Task_create(menuTask, &menuParams, NULL);
-	if(menuTaskVar == NULL)
-	{
+		if(menuTaskVar == NULL)
+		{
 		System_abort("menuTask failed");
-	}
-	*/
-    //Init6LoWPAN(); // This function call before use!
+		}
+	 */
+	//Init6LoWPAN(); // This function call before use!
 
-    //commTask = Task_create(commTaskFxn, &commTaskParams, NULL);
-    //if (commTask == NULL) {
-    //	System_abort("Task create failed!");
-    //}
+	//commTask = Task_create(commTaskFxn, &commTaskParams, NULL);
+	//if (commTask == NULL) {
+	//	System_abort("Task create failed!");
+	//}
 
-    /* Sanity check */
-    System_printf("Hello world!\n");
-    System_flush();
-    
-    /* Start BIOS */
-    BIOS_start();
+	/* Sanity check */
+	System_printf("Hello world!\n");
+	System_flush();
 
-    return (0);
+	/* Start BIOS */
+	BIOS_start();
+
+	return (0);
 }
